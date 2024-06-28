2812a6b24ff2cd86a6a9059a4fb3c0aad3ec3ff9
sjake12 <apostol.sjake12@gmail.com>
2024-06-28 15:15:58 +0800

    Random Emojis added

---
+++ 21. Random Emojis/script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("#emoji");
+
+const emojis = ["😊", "😂", "🤣", "😍", "😒", "😘", "😁", "😉", "😅", "🤩"];
+
+btn.addEventListener("mouseover", () => {
+  btn.innerHTML = emojis[Math.floor(Math.random() * emojis.length)];
+});
