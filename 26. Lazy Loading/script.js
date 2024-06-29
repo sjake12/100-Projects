c2cf5db476cbc30408818912c515d7f788aab6fe
sjake12 <apostol.sjake12@gmail.com>
2024-06-29 14:48:48 +0800

    change added

--- 26. Lazy Loading/script.js
+++ 26. Lazy Loading/script.js
@@ -10,6 +10,7 @@ function blurring() {
   if (load > 99) {
     clearInterval(int);
   }
+
   loadText.innerText = `${load}%`;
   loadText.style.opacity = scale(load, 0, 100, 1, 0);
   background.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
